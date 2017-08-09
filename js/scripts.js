4254b9c5124c91d40c62b68664de1d3e8820f207
Debbie Michel and Linda Luu <debbie@>
2017-08-09 13:45:38 -0700

    Add subtract, multiply and divide for calculations

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,45 @@ $(document).ready(function() {
     event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
-
-      // var result =add(number1, number2);
+       var result =add(number1, number2);
+       $("#output").text(result);
       // alert(result);
-      alert(add(number1, number2));
+      //alert(add(number1, number2));
+      $("form#sub").submit(function(event){
+        event.preventDefault();
+      var number1 = parseInt($("#sub1").val());
+      var number2 = parseInt($("#sub2").val());
+
+     var result =subtract(number1, number2);
+     $("#output1").text(result);
+
+     $("form#mult").submit(function(event){
+       event.preventDefault();
+     var number1 = parseInt($("#mult1").val());
+     var number2 = parseInt($("#mult2").val());
+
+      var result =multiply(number1, number2);
+      $("#output2").text(result);
+
+      $("form#div").submit(function(event){
+        event.preventDefault();
+      var number1 = parseInt($("#div1").val());
+      var number2 = parseInt($("#div2").val());
+
+       var result =divide(number1, number2);
+       $("#output3").text(result);
+
+
+            });
+
+           });
+
+
+
+
+      });
+
+
+
     });
 });
