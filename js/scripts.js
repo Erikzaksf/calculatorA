c37a7a5fef25fd37bb7ebe63b8e04dc44a286cfa
Erikzaksf <erik.zak.sf@gmail.com>
2017-08-10 13:53:34 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,49 +19,21 @@
 //Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("form#calculator").submit(function() {
     event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-       var result =add(number1, number2);
-       $("#output").text(result);
-      // alert(result);
-      //alert(add(number1, number2));
-      $("form#sub").submit(function(event){
-        event.preventDefault();
-      var number1 = parseInt($("#sub1").val());
-      var number2 = parseInt($("#sub2").val());
-
-     var result =subtract(number1, number2);
-     $("#output1").text(result);
-
-     $("form#mult").submit(function(event){
-       event.preventDefault();
-     var number1 = parseInt($("#mult1").val());
-     var number2 = parseInt($("#mult2").val());
-
-      var result =multiply(number1, number2);
-      $("#output2").text(result);
-
-      $("form#div").submit(function(event){
-        event.preventDefault();
-      var number1 = parseInt($("#div1").val());
-      var number2 = parseInt($("#div2").val());
-
-       var result =divide(number1, number2);
-       $("#output3").text(result);
-
-
-            });
-
-           });
-
-
-
-
-      });
-
-
-
-    });
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
 });
